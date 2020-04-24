f66b32efacae9d6fa416bbb0e32a8a38850214c0
Yogendrasingh-Rathore <53259324+Yogendrasingh-Rathore@users.noreply.github.com>
2020-04-24 16:36:18 +0530

    Update dockerlib.groovy

--- vars/dockerlib.groovy
+++ vars/dockerlib.groovy
@@ -1,6 +1,6 @@
 def call(String image_name)
 {  
-  withDockerContainer(image: ${image_name})
+  withDockerContainer(image: image_name)
      { 
          echo "Container created with ${image_name}"
          echo "starting with clean up ..."
